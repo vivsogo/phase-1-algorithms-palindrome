37013c41beb8dfbb3dcb8c40c41162efa08b166e
Vivian Gomez <viviangomez0@gmail.com>
2021-12-14 15:11:27 -0500

    compltete

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome() {
+  var re = /[\W_]/g;
+  var lowRegStr = str.toLowerCase().replace(re, '');
+  var reverseStr = lowRegStr.split('').reverse().join(''); 
+  return reverseStr === lowRegStr; // Write your algorithm here
 }
-
+isPalindrome("race car")
 /* 
+  // if name = name ? return true 
+  //else if name / name return false 
   Add your pseudocode here
 */
 
 /*
   Add written explanation of your solution here
+  the /[\W_]/g removes all non alphanumeric characters 
+  strinf.lower.replace will make everythigng lower case 
+  resevers string 
 */
 
 // You can run `node index.js` to view these console logs
-if (require.main === module) {
+//if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+//  console.log("Expecting: true");
+  //console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  //console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  //console.log("Expecting: false");
+  //console.log("=>", isPalindrome("robot"));
 }
 
-module.exports = isPalindrome;
+//module.exports = isPalindrome;
